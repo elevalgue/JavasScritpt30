42afb6ec06202fcd5261f7108a0fbc5537de3ec4
elevalgue <elena.valenzuela.guerrero@gmail.com>
2020-11-12 20:16:41 +0100

    Add functionalities

--- 11-custom-html5-video-player/js/main.js
+++ 11-custom-html5-video-player/js/main.js
@@ -10,3 +10,33 @@ const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
 /* Build out functions */
+function togglePlay() {
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+}
+
+function updateButton() {
+    const icon = this.paused ? '►' : '❚ ❚';
+    console.log(icon);
+    toggle.textContent = icon; 
+}
+
+function skip() {
+    video[this.name] = this.value;
+}
+
+function handleRangeUpdate() {
+    video[this.name] = this.value; 
+}
+
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100; 
+    progressBar.getElementsByClassName.flexBasis = `${percent}%`;    
+}
+
+function scrub(event) {
+    const scrubTime = (event.offsetx / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime; 
+}
+
+
