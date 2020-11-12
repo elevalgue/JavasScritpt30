459fda831c38eb4030d5f814504692a3f6ac5e60
elevalgue <elena.valenzuela.guerrero@gmail.com>
2020-11-12 20:17:33 +0100

    Add listeners

--- 11-custom-html5-video-player/js/main.js
+++ 11-custom-html5-video-player/js/main.js
@@ -1,6 +1,7 @@
 'use strict'; 
 
 /* Get Our Elements */
+
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
@@ -10,6 +11,7 @@ const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
 /* Build out functions */
+
 function togglePlay() {
     const method = video.paused ? 'play' : 'pause';
     video[method]();
@@ -39,4 +41,20 @@ function scrub(event) {
     video.currentTime = scrubTime; 
 }
 
+/* Hook up the event listeners */
 
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
+toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
