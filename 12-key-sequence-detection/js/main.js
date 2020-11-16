f844dab9c819978d2f2d243f28209d6b9edc0820
elevalgue <elena.valenzuela.guerrero@gmail.com>
2020-11-16 23:10:59 +0100

    Add functionality

--- 12-key-sequence-detection/js/main.js
+++ 12-key-sequence-detection/js/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const pressed = [];
+const secretCode = 'pandepita';
+
+window.addEventListener('keyup', (event) => {
+    console.log(event.key);
+    pressed.push(event.key);
+    pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+    if (pressed.join('').includes(secretCode)) {
+        console.log('DING DING DING!');
+        cornify_add();
+    }
+    console.log(pressed);
+});
