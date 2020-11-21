39acf463235935dffa77984cad199219d6ebf240
elevalgue <elena.valenzuela.guerrero@gmail.com>
2020-11-21 22:40:27 +0100

    Finish exercise

--- 16-css-text-shadow-mouse-over-effect/js/main.js
+++ 16-css-text-shadow-mouse-over-effect/js/main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const hero = document.querySelector('.hero');
+  const text = hero.querySelector('h1');
+  const walk = 500; // 500px
+  
+  function shadow(event) {
+    const { offsetWidth: width, offsetHeight: height } = hero;
+      let { offsetX: x, offsetY: y } = event;
+
+      if (this !== event.target) {
+          x = x + event.target.offsetLeft;
+          y = y + event.target.offsetTop;
+      }
+
+      const xWalk = Math.round((x / width * walk) - (walk / 2));
+      const yWalk = Math.round((x / width * walk) - (walk / 2));
+
+      text.style.textShadow = ` ${xWalk}px ${yWalk}px 0 rgba(255,0,255,0.7),
+      ${xWalk * -1}px ${yWalk}px 0 rgba(0,255,255,0.7),
+      ${yWalk}px ${xWalk * -1}px 0 rgba(0,255,0,0.7),
+      ${yWalk * -1}px ${xWalk}px 0 rgba(0,0,255,0.7) `;
+
+    }
+  
+    hero.addEventListener('mousemove', shadow);
