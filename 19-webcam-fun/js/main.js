614edb0a057da36b7156fc3ecd40d53c36cc6819
elena <elena.valenzuela.guerrero@gmail.com>
2022-05-20 13:20:45 +0200

    Add stylesheet

--- 19-webcam-fun/js/main.js
+++ 19-webcam-fun/js/main.js
@@ -6,3 +6,26 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+  navigator.mediaDevices
+    .getUserMedia({ video: true, audio: false })
+    .then((localMediaStream) => {
+      console.log(localMediaStream);
+      video.srcObject = localMediaStream;
+      video.play();
+    })
+    .catch((err) => {
+      console.error(`OH NO!!!`, err);
+    });
+}
+
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  console.log(width, height);
+  canvas.width = width;
+  canvas.height = height;
+}
+
+getVideo();
