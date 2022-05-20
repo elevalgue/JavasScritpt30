5426f8ad60f7fae9adb0a14eb55f5251c74197f3
elena <elena.valenzuela.guerrero@gmail.com>
2022-05-20 11:16:43 +0200

    Add snap file

--- 19-webcam-fun/js/main.js
+++ 19-webcam-fun/js/main.js
@@ -1,4 +1,8 @@
 'use strict';
 console.log('Are you ready for a good time');
 
-
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
